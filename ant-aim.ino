c838eed6faf34d5887ca1099f072fb781ee2ddd3
Arnau478 <arnauxabia@gmail.com>
2022-05-19 21:09:34 +0200

    Updated configuration parameters (pins, sensibility, etc.)

--- ant-aim.ino
+++ ant-aim.ino
@@ -1,6 +1,6 @@
 #include <Servo.h>
 
-#define SENS 100
+#define SENS 25
 #define DISC (SENS/10000.0)
 
 Servo servoX;
@@ -13,24 +13,10 @@ bool manual = true;
 
 bool btnPressedLast = true;
 
-void setup(){
-  servoX.attach(6);
-  servoY.attach(5);
-  servoX.write(0);
-  servoY.write(0);
-  delay(1000);
-  for(int i = 0; i < 180; i++){
-    servoX.write(i);
-    servoY.write(i);
-    delay(5);
-  }
-  delay(500);
-  for(int i = 180; i > 0; i--){
-    servoX.write(i);
-    servoY.write(i);
-    delay(5);
-  }
-  delay(1000);
+void setup() {
+  servoX.attach(7);
+  servoY.attach(8);
+  delay(3000);
   servoX.write(90);
   servoY.write(90);
 }
@@ -41,22 +27,22 @@ void loop(){
     if(!btnPressedLast && btn) manual = !manual;
     btnPressedLast = btn;
   }
-  
-  digitalWrite(7, manual);
-  if(manual){
-    float yawI = map(analogRead(A0), 0, 1023, -SENS, SENS)/1000.0;
-    float pitchI = map(analogRead(A1), 0, 1023, -SENS, SENS)/1000.0;
-    if(yawI < -DISC || yawI > DISC) yaw += yawI;
-    if(pitchI < -DISC || pitchI > DISC) pitch += pitchI;
+
+  digitalWrite(10, manual);
+  if (manual) {
+    float yawI = map(analogRead(A0), 0, 1023, -SENS, SENS) / 1000.0;
+    float pitchI = map(analogRead(A1), 0, 1023, -SENS, SENS) / 1000.0;
+    if (yawI < -DISC || yawI > DISC) yaw += yawI;
+    if (pitchI < -DISC || pitchI > DISC) pitch += pitchI;
   }
   else{
     float destYaw = 90;
-    float destPitch = millis()/100 % 180;
-    yaw += (destYaw-yaw)/2000.0;
-    pitch += (destPitch-pitch)/2000.0;
+    float destPitch = millis() / 100 % 180;
+    yaw += (destYaw - yaw) / 5000.0;
+    pitch += (destPitch - pitch) / 5000.0;
   }
   yaw = constrain(yaw, 0, 180);
   pitch = constrain(pitch, 0, 180);
   servoX.write((int)yaw);
-  servoY.write(map((int)pitch, 0, 180, 10, 180));
+  servoY.write(map((int)pitch, 0, 180, 0, 180));
 }
