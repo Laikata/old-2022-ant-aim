b4b112ab6eaf577d3c5f9d681809fb00c1781964
Josep Mengual <salmon@truita.es>
2022-05-21 12:14:16 +0200

    Echo received data

--- comms.cpp
+++ comms.cpp
@@ -22,42 +22,52 @@ void rev_float(float *value) {
     f32_u32.i = __builtin_bswap32(f32_u32.i);
 }
 
-//Returns true if there's a new packet
+// Reads from Serial while echoing
+size_t echo(uint8_t *buffer, int length) {
+  size_t result = Serial.readBytes(buffer, length);
+  Serial.write(buffer, result);
+  return result;
+}
+
+// Returns true if there's a new packet
 bool comms_recv(vec3 *pos) {
     uint8_t packet[20];
     if(Serial.available()){
-        Serial.readBytes(packet, 1);
+        echo(packet, 1);
         if(packet[0] == 0x16) {
             const uint8_t data_size = 13;
-            Serial.readBytes(packet + 1, 3);
+            echo(packet + 1, 3);
 
             if(packet[1] != data_size || packet[3] != 0x01) 
                 return false;
 
-            Serial.readBytes(packet + 4, 12);
-            memcpy(pos, packet + 4, 4);
-            memcpy(pos, packet + 8, 4);
-            memcpy(pos, packet + 12, 4);
-
-            #if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
-                rev_float(&pos->x);
-                rev_float(&pos->y);
-                rev_float(&pos->z);
-            #elif __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
-
-            #else
-            #error Unsupported endianness
-            #endif
+            echo(packet + 4, 12);
             
-            Serial.readBytes(packet + 16, 4);
+            // We have to read the checksum before writing to pos
+            echo(packet + 16, 4);
             uint32_t checksum = 
                     ( ((uint32_t) packet[16]) << 24) +
                     ( ((uint32_t) packet[17]) << 16) +
                     (packet[18] << 8) +
                     packet[19];
 
-            if(crc32(packet + 3, 13) == checksum) 
-                return true;
+            if(crc32(packet + 3, 13) == checksum) {
+              memcpy(pos, packet + 4, 4);
+              memcpy(pos, packet + 8, 4);
+              memcpy(pos, packet + 12, 4);
+  
+              #if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+                  rev_float(&pos->x);
+                  rev_float(&pos->y);
+                  rev_float(&pos->z);
+              #elif __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+  
+              #else
+              #error Unsupported endianness
+              #endif
+              
+              return true;
+            }
         }
     }
     return false;
