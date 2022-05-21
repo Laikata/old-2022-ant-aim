8d2b3a759a6395ac5f939b047bf0bab60cb448ae
Arnau478 <arnauxabia@gmail.com>
2022-05-21 11:58:16 +0200

    Yesss

--- ant-aim.ino
+++ ant-aim.ino
@@ -1,48 +1,81 @@
 #include <Servo.h>
 
-#define SENS 25
+#define SENS 500
 #define DISC (SENS/10000.0)
+
+typedef struct {
+  float x;
+  float y;
+  float z;
+} vec3;
 
 Servo servoX;
 Servo servoY;
 
-float pitch = 0; // 0 to 180
-float yaw = 0; // 0 to 180
+float pitch = 45; // 0 to 180
+float yaw = 90; // 0 to 180
+float realPitch = pitch;
+float realYaw = yaw;
 
-bool manual = true;
-
-bool btnPressedLast = true;
-
-void setup() {
-  servoX.attach(7);
-  servoY.attach(8);
-  delay(3000);
-  servoX.write(90);
-  servoY.write(90);
+vec3 base_pos(){
+  vec3 vec;
+  vec.x = 0;
+  vec.y = 0;
+  vec.z = 0;
+  return vec;
 }
 
-void loop(){
-  {
-    bool btn = digitalRead(8);
-    if(!btnPressedLast && btn) manual = !manual;
-    btnPressedLast = btn;
-  }
+vec3 dest_pos(){
+  vec3 vec;
+  vec.x = 0;
+  vec.y = 0;
+  vec.z = 1;
+  return vec;
+}
 
-  digitalWrite(10, manual);
-  if (manual) {
-    float yawI = map(analogRead(A0), 0, 1023, -SENS, SENS) / 1000.0;
-    float pitchI = map(analogRead(A1), 0, 1023, -SENS, SENS) / 1000.0;
-    if (yawI < -DISC || yawI > DISC) yaw += yawI;
-    if (pitchI < -DISC || pitchI > DISC) pitch += pitchI;
+void setup() {
+  Serial.begin(9600);
+  servoX.write(90);
+  servoX.write(90);
+  servoX.attach(7);
+  servoY.attach(8);
+  delay(5000);
+  for(int i = 0; i < 100; i++){
+    servoX.write(((90*(100-i))+(yaw*i))/100);
+    servoY.write(((90*(100-i))+(pitch*i))/100);
+    delay(10);
   }
-  else{
-    float destYaw = 90;
-    float destPitch = millis() / 100 % 180;
-    yaw += (destYaw - yaw) / 5000.0;
-    pitch += (destPitch - pitch) / 5000.0;
-  }
+  delay(1000);
+}
+
+void loop() {
+  // Auto-aim
+  vec3 base = base_pos();
+  vec3 dest = dest_pos();
+  yaw = atan((dest.y-base.y)/(dest.x-base.x))*180/PI;
+  pitch = atan((dest.z-base.z)/sqrt(dest.x*dest.x+dest.y*dest.y))*180/PI;
+
+  // Manual joystick adjustment
+  float yawI = 0;
+  float pitchI = 0;
+  if(analogRead(A0) > 5) yawI = map(analogRead(A0), 0, 1023, -SENS, SENS) / 1000.0;
+  if(analogRead(A1) > 5) pitchI = map(analogRead(A1), 0, 1023, -SENS, SENS) / 1000.0;
+  if (yawI < -DISC || yawI > DISC) yaw += yawI;
+  if (pitchI < -DISC || pitchI > DISC) pitch += pitchI;
+
+  // Write constrained values to servos 
   yaw = constrain(yaw, 0, 180);
   pitch = constrain(pitch, 0, 180);
-  servoX.write((int)yaw);
-  servoY.write(map((int)pitch, 0, 180, 0, 180));
+  realYaw += (yaw-realYaw) / 100;
+  realPitch += (pitch-realPitch) / 100;
+  servoX.write(realYaw);
+  servoY.write(realPitch);
+  Serial.print(yaw);
+  Serial.print(", ");
+  Serial.print(pitch);
+  Serial.print(", ");
+  Serial.print(realYaw);
+  Serial.print(", ");
+  Serial.print(realPitch);
+  Serial.print("\n");
 }
