d0790b2b3a348cec7c4aea8582ddef88f8347f98
Josep Mengual <salmon@truita.es>
2022-05-22 20:55:09 +0200

    Remove unused variables

--- ant-aim.ino
+++ ant-aim.ino
@@ -12,19 +12,6 @@ SoftwareSerial ss(2,3);
 Servo servoX;
 Servo servoY;
 
-float pitch = 45; // 0 to 180
-float yaw = 90; // 0 to 180
-float realPitch = pitch;
-float realYaw = yaw;
-
-vec3 base_pos(){
-  vec3 vec;
-  vec.x = 0;
-  vec.y = 0;
-  vec.z = 0;
-  return vec;
-}
-
 void setup() {
   Serial.begin(9600);
   ss.begin(9600);
@@ -33,12 +20,6 @@ void setup() {
   servoX.write(90);
   servoY.write(90);
   delay(5000);
-  for(int i = 0; i < 100; i++){
-    servoX.write(((90*(100-i))+(yaw*i))/100);
-    servoY.write(((90*(100-i))+(pitch*i))/100);
-    delay(10);
-  }
-  delay(1000);
 }
 
 void loop() {
@@ -49,13 +30,6 @@ void loop() {
   yaw = atan((dest.y-base.y)/(dest.x-base.x))*180/PI;
   pitch = atan((dest.z-base.z)/sqrt(dest.x*dest.x+dest.y*dest.y))*180/PI;
 
-  // Manual joystick adjustment
-  float yawI = 0;
-  float pitchI = 0;
-  if(analogRead(A0) > 5) yawI = map(analogRead(A0), 0, 1023, -SENS, SENS) / 1000.0;
-  if(analogRead(A1) > 5) pitchI = map(analogRead(A1), 0, 1023, -SENS, SENS) / 1000.0;
-  if (yawI < -DISC || yawI > DISC) yaw += yawI;
-  if (pitchI < -DISC || pitchI > DISC) pitch += pitchI;
 
   // Write constrained values to servos 
   yaw = constrain(yaw, 0, 180);
